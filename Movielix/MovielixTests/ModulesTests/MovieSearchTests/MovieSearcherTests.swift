2230126081dec4c77708757cdacaba6b173a0e7f
mustafa <mustafa.fci.cs@gmail.com>
2020-05-08 19:55:40 +0200

    test: unit test for searching prefix, infix and postfix

---
+++ Movielix/MovielixTests/ModulesTests/MovieSearchTests/MovieSearcherTests.swift
@@ -0,0 +1,44 @@
+//
+//  MovieSearcher.swift
+//  MovielixTests
+//
+//  Created by Mustafa Ezzat on 5/8/20.
+//  Copyright © 2020 Swvl. All rights reserved.
+//
+
+import XCTest
+@testable import Movielix
+
+class MovieSearcherTests: XCTestCase {
+    var sut: MovieSearcher!
+    
+    override func setUp() {
+        super.setUp()
+        // should init after super.setup()
+        sut = MovieSearcher()
+    }
+
+    override func tearDown() {
+        // should deinit before super.tearDown()
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_MovieSearcher_Query_Prefix() {
+        let keyword = "The one"
+        let result = sut.query(list: ConstantTests.categorizedMovies, keyword: keyword)
+        XCTAssertEqual(result.description, ConstantTests.prefixMovieWithTheOne.description)
+    }
+    
+    func test_MovieSearcher_Query_Postfix() {
+        let keyword = "Madea Halloween"
+        let result = sut.query(list: ConstantTests.categorizedMovies, keyword: keyword)
+        XCTAssertEqual(result.description, ConstantTests.postfixMovieWithMadeaHalloween.description)
+    }
+    
+    func test_MovieSearcher_Query_Infix() {
+        let keyword = "Bob\'s Super Groovy"
+        let result = sut.query(list: ConstantTests.categorizedMovies, keyword: keyword)
+        XCTAssertEqual(result.description, ConstantTests.infixMovieBobSuperGroovy.description)
+    }
+}

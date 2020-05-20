689e5381322a3140a9fc8d568d2d3410434358ab
mustafa <mustafa.fci.cs@gmail.com>
2020-05-21 00:51:14 +0200

    feedback: use compactMap when call query

--- Movielix/Movielix/Modules/MovieSearch/Worker/MovieSearcher.swift
+++ Movielix/Movielix/Modules/MovieSearch/Worker/MovieSearcher.swift
@@ -17,17 +17,16 @@ protocol Searcher {
 class MovieSearcher: Searcher {
     typealias Element = MoviesPerYear
     typealias Keyword = String
+    func moviesPerYear(moviesPerYear: MoviesPerYear, keyword: String) -> MoviesPerYear? {
+        guard let movies = moviesPerYear.movies else {
+            return nil
+        }
+        let atMostTopRatedFive = movies.filter {$0.isExist(keyword)}.prefix(5)
+        return atMostTopRatedFive.isEmpty ? nil : MoviesPerYear(year: moviesPerYear.year, movies: Array(atMostTopRatedFive))
+    }
     func query(list: [MoviesPerYear], keyword: String) -> [MoviesPerYear] {
-        var filterdResults = [MoviesPerYear]()
-        for category in list {
-            guard let movies = category.movies else {
-                continue
-            }
-            let atMostTopRatedFive = movies.filter {$0.isExist(keyword)}.prefix(5)
-            if !atMostTopRatedFive.isEmpty {
-                let filteredCategory = MoviesPerYear(year: category.year, movies: Array(atMostTopRatedFive))
-                filterdResults.append(filteredCategory)
-            }
+        let filterdResults = list.compactMap {
+            moviesPerYear(moviesPerYear: $0, keyword: keyword)
         }
         return filterdResults
     }
